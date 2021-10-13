974d3d65dd98a8af6f03f3e707ce36ce598bdba1
Chroma72 <frobo1@outlook.com>
2021-10-13 07:11:25 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -6,3 +6,24 @@ const __dirname = path.resolve()
 
 const io = geckos()
 io.listen()
+io.onConnection(channel => {
+  
+  io.onRaw(data => {
+    
+    let packet = Packet.fromClientData(data)
+    
+    switch (packet.opcode) {
+      case 0x01:
+        break;
+        
+      case 0x02:
+        break;
+        
+      default:   
+    }
+    
+    
+    
+  }
+  
+})
