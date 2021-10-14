584604b70f35b19f8efd3e42307f74f446a877f8
Chroma72 <frobo1@outlook.com>
2021-10-14 13:01:26 -0500

    Update packet.mjs

--- shared/packet.mjs
+++ shared/packet.mjs
@@ -1,1 +1,23 @@
+export default Packet {
 
+  constructor (opcode, size) {
+    let buffer = new ArrayBuffer(1 + size)
+    this.view = new DataView(buffer)
+    this.view.setUint8(0, opcode)
+  }
+  
+  static fromData(buffer) {
+    let packet = Object.create(this.prototype)
+    packet.view = (!buffer.buffer) ? new DataView(buffer) : new DataView(buffer.buffer)
+    return packet
+  }
+  
+  get opcode() {
+    return this.view.getUint8(0)
+  }
+  
+  get payload() {
+    return this.view.buffer
+  }
+  
+}
